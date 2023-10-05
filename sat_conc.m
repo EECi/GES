4faf768a3cbb43d284dedc2f0c5ee6f28f132b96
rmward61 <rmw61@cam.ac.uk>
2023-10-05 10:14:57 +0100

    Changes to directory structure

--- sat_conc.m
+++
@@ -1,9 +0,0 @@
-function a = sat_conc(T)
-
-T = T - 273.15;
-
-spec_hum = exp(11.56 - 4030./(T + 235)); % Ratio of mass of water vapour to mass of dry air (ref?)
-air_dens = -0.0046*T + 1.2978; % Density of air [kg/m^3]
-a = spec_hum.*air_dens; % Density of water vapour [kg/m^3]
-
-end
